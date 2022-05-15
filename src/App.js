21bf3fedf60b2180925274960f8d847b29d3dfb2
Angela Balsamo <cuoreangela@hotmail.it>
2022-05-15 12:21:59 +0200

    prop 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { HelloWorld } from "./HelloWorld";
+import { Welcome } from "./Welcome";
 
 export class App extends React.Component {
     render () {
-        return <div><HelloWorld /></div>
-    }
+        return (
+        <div>
+            <HelloWorld />
+            <Welcome name="John" />
+        </div>
+    )}
 }
--- src/Message.js
+++ src/Message.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { render } from "react-dom";
 
 export class Message extends React.Component {
     render () {
