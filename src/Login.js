6037da1ce11d5efba158b7d8cad27c2dd06b67fe
Angela Balsamo <cuoreangela@hotmail.it>
2022-05-18 14:34:11 +0200

    da finire

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,8 @@ export class Login extends React.Component {
     state = {
         username: '',
         password: '',
-        remember: false
+        remember: false,
+        disabled: true
     }
 
     handleLogin = (event) => {
@@ -18,12 +19,21 @@ export class Login extends React.Component {
         })
     }
 
+    onLogin = () => {
+
+        this.setState ({
+            disabled: (this.state.username.length <1 && this.state.password.length<1)  ? true : false
+        })
+    }
+
     render () {
         return (
             <div>
+                {this.onLogin}
                 <input onChange={this.handleLogin} name="username" value={this.state.username}></input>
                 <input onChange={this.handleLogin} name="password" type="password" value={this.state.password}></input>
                 <input onChange={this.handleLogin} name="remember" type="checkbox" checked={this.state.remember}></input>
+                <button onLoad={this.onLogin} disabled={this.state.disabled}>Login</button>
             </div>        
         )
     }
