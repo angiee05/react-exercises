57b68550102269486d201f86746b2b8048a715e9
Angela Balsamo <cuoreangela@hotmail.it>
2022-05-25 01:00:09 +0200

    list 3

--- src/TodoList.js
+++ src/TodoList.js
@@ -6,12 +6,6 @@ export class TodoList extends React.Component {
         input: ''
     }
 
-    componentDidMount = () => {
-        this.setState({
-            items: this.state.items.map((item) => <li>{item}</li>)
-        })
-    }
-
     handleInput = (event) => {
         const value = event.target.value
 
@@ -21,18 +15,20 @@ export class TodoList extends React.Component {
     }
 
     addTodo = () => {
-        this.state.items.push(<li>{this.state.input}</li>)
+        this.setState({
+            itmes: this.state.items.push(this.state.input)
+        })
     }
 
     render() {
         return (
             <div>
                 <div>
-                    <ul>{this.state.items}</ul>
+                    <ul>{this.state.items.map((item,index) => <li key={index}>{item}</li>)}</ul>
                 </div>
                 <div>
                     <p>Inserisci un nuovo todo alla lista:</p>
-                    <input onChange={this.handleInput}></input>
+                    <input onChange={this.handleInput} value={this.state.input}></input>
                     <button onClick={this.addTodo}>Aggiungi</button>
                 </div>
             </div>
