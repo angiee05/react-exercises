29e58fff07ed92ed55f8e24d81da15b76fb29739
Angela Balsamo <cuoreangela@hotmail.it>
2022-05-27 22:30:52 +0200

    context 2

--- src/DisplayLanguage.js
+++ src/DisplayLanguage.js
@@ -1,21 +1,12 @@
 import React from 'react'
 import { LanguageContext } from './LanguageContext'
 
-const Strings = {
-    en:{
-        sayHi: "Hi"
-    },
-    it:{
-        sayHi: "Ciao"
-    }
-}
-
 export class DisplayLanguage extends React.Component {
     render() {
         return (
             <div>
                 <LanguageContext.Consumer>
-                    { Language => Strings[Language].sayHi}                      
+                    { Language => <h1>{Language}</h1>}                      
                 </LanguageContext.Consumer>
             </div>
         )
