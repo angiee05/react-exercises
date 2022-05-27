49faed5105d17f7708fd42dc90b1a7f8ce3a605d
Angela Balsamo <cuoreangela@hotmail.it>
2022-05-27 23:36:41 +0200

    function components 3

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export class App extends React.Component {
     render() {
         return (
             <div>
-                <Sum numbers={[1,2,3,4]}/>                               
+                <Sum />                               
             </div>
         )
     }
--- src/Sum.js
+++ src/Sum.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Sum({ numbers }) {
+export default function Sum({ numbers = [1,2,3,4] }) {
   return (
     <div>
         <h1>La somma è:{numbers.reduce((a,b) => a+b)}</h1>
