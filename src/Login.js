88adaff85b53434dbac7c35166280f234409469f
Angela Balsamo <cuoreangela@hotmail.it>
2022-06-02 13:58:33 +0200

    custom hooks 2

--- src/Login.js
+++ src/Login.js
@@ -1,41 +1,53 @@
 import React, { useState } from 'react'
 
-export default function Login() {
+function useLogin() {
     const [data, setData] = useState({
         username: '',
         password: '',
-        remember: false
     })
 
     const handleLogin = (event) => {
         const value = event.target.value
         const name = event.target.name
-        const type = event.target.type
-        const checked = event.target.checked
 
         setData((data) => {
-            return{
+            return {
                 ...data,
-                [name]: type === "checkbox" ? checked : value   
+                [name]: value
             }
         })
     }
 
-    const handleReset = () => {
-        setData({
-            username: '',
-            password: '',
-            remember: false
-        })
+    return {
+        data: data,
+        onLogin: handleLogin
     }
+}
+
+export default function Login() {
+    const { data, onLogin } = useLogin({
+        username: '',
+        password: '',
+    })
+
+
+
+
+    // const handleReset = () => {
+    //     setData({
+    //         username: '',
+    //         password: '',
+    //         remember: false
+    //     })
+    // }
 
     return (
         <div>
-            <input onChange={handleLogin} name="username" value={data.username}></input>
-            <input onChange={handleLogin} name="password" type="password" value={data.password}></input>
-            <input onChange={handleLogin} name="remember" type="checkbox" checked={data.remember}></input>
-            <button onClick={handleReset}>Reset</button>
-            <button disabled={!(data.username && data.password)}>Login</button>
+            <input onChange={onLogin} name="username" value={data.username}></input>
+            <input onChange={onLogin} name="password" type="password" value={data.password}></input>
+            {/* <input onChange={handleLogin} name="remember" type="checkbox" checked={data.remember}></input> */}
+            {/* <button onClick={handleReset}>Reset</button>
+            <button disabled={!(data.username && data.password)}>Login</button> */}
         </div>
     )
 }
