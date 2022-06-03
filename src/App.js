79bbd7ab8454b6c1173e858f81982b4a16067f61
Angela Balsamo <cuoreangela@hotmail.it>
2022-06-04 00:34:19 +0200

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import ClickCounter from "./ClickCounter";
 import GithubUser from "./GithubUser";
 export class App extends React.Component {
 
     render() {
         return (
             <div>
-                <GithubUser username="angiee05"/>                               
+                <ClickCounter />                               
             </div>
         )
     }
--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 
 function useCounter(initialValue = 0) {
     const[counter, setCounter] = useState(initialValue)
 
-    const handleCounterIncrement = (() => {
+    const handleCounterIncrement = useCallback(function handleCounterIncrement() {
         setCounter((c) => c + 1)
-    })
+    }, [])
 
-    const handleCounterDecrement =(() => {
+    const handleCounterDecrement = useCallback(function handleCounterDecrement() {
         setCounter((c) => c -1)
-    })
+    }, [])
 
-    const handleCounterReset = (() => {
+    const handleCounterReset = useCallback(function handleCounterReset () {
         setCounter(initialValue)
-    })
+    }, [])
 
     return {
         counter: counter,
