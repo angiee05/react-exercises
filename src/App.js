1c35dc0a38610769891bee18e1f278439efca100
Angela Balsamo <cuoreangela@hotmail.it>
2022-06-04 02:31:50 +0200

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import ClickCounter from "./ClickCounter";
-import GithubUser from "./GithubUser";
+import FilteredList from "./FilteredList";
 export class App extends React.Component {
 
     render() {
         return (
             <div>
-                <ClickCounter />                               
+                <FilteredList name1="jimmy" age1={25} id1={1} name2="john" age2={11} id2={2} name3="billy" age3={30} id3={3}/>                               
             </div>
         )
     }
---
+++ src/FilteredList.js
@@ -0,0 +1,16 @@
+import React, { useMemo } from 'react'
+
+
+export default function FilteredList(props) {
+    const persons = [
+        {name: props.name1, id: props.id1, age: props.age1},
+        {name: props.name2, id: props.id2, age: props.age2},
+        {name: props.name3, id: props.id3, age: props.age3}
+    ]
+    
+    const personsFiltered = useMemo(() => persons.filter((person) => person.age > 18 ), [])
+
+    return (
+        <ul>{personsFiltered.map((person) => <div><p>name: {person.name}, age: {person.age}, id: {person.id}</p> </div>)}</ul>
+    )
+}
