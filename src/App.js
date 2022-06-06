be92d948da547ec0dab9a9bc832f22ced0b736a6
Angela Balsamo <cuoreangela@hotmail.it>
2022-06-06 16:23:04 +0200

    react router 4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import Counter from './Counter'
 import ShowGithubUser from './ShowGithubUser'
 import Welcome from './Welcome'
 
 export default function App() {
     return (
-        <Routes>
-            <Route path='/' element={<Welcome name="John" age="20" />} />
-            <Route path='/counter' element={<Counter />} />
-            <Route path='users/:username' element={<ShowGithubUser />} />
-        </Routes>
+        <div>
+            <div>
+                <Link to='/'>Go to welcome page</Link> | <Link to='/counter'>Go to counter page</Link> | <Link to='users/:username'>Go to GithubUser page</Link>
+            </div>
+            <Routes>
+                <Route path='/' element={<Welcome name="John" age="20" />} />
+                <Route path='/counter' element={<Counter />} />
+                <Route path='users/:username' element={<ShowGithubUser />} />
+            </Routes>
+        </div>
     )
 }
