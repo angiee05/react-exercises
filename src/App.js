6a4f196bb5a00424b2ce7b1df42cb9eb0a45eb00
Angela Balsamo <cuoreangela@hotmail.it>
2022-06-06 13:09:51 +0200

    react router 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Counter from './Counter'
 import Welcome from './Welcome'
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Welcome name="John" age="20" />} />
+                <Route path='/counter' element={<Counter />} />
             </Routes>
         </BrowserRouter>
     )
