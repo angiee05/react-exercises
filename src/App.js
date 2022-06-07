31df60c64d62a48dc57c0b855bc289825e9c9083
Angela Balsamo <cuoreangela@hotmail.it>
2022-06-07 14:00:24 +0200

    swr 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react'
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
-import Counter from './Counter'
 import GithubUser from './GithubUser'
-import GithubUserList from './GithubUserList'
-import ShowGithubUser from './ShowGithubUser'
-import Welcome from './Welcome'
 
 export default function App() {
     return (
-        <GithubUser username={"angiee05"} />
+        <GithubUser username={'angiee05'} />
     )
 }
